f79db825987af4019076f85cf149a8a658b847b7
Christian <115067566+kitearuba@users.noreply.github.com>
2024-11-26 19:40:54 +0100

    1126-1940 Updated return to pass norm

--- src/sorting/sort_helper.c
+++ src/sorting/sort_helper.c
@@ -3,64 +3,59 @@
 int find_min(t_stack *stack)
 {
 	t_list	*current;
-    int min_value;
+	int		min_value;
 
-    if (!stack || !stack->top)
-        return (INT_MAX); // Return INT_MAX if the stack is empty
-
-    current = stack->top;
-    min_value = *(int *)current->content;
-
-    while (current)
-    {
-        if (*(int *)current->content < min_value)
-            min_value = *(int *)current->content;
-        current = current->next;
-    }
-    return (min_value);
+	if (!stack || !stack->top)
+		return (-1); // Return an invalid value if the stack is empty
+	current = stack->top;
+	min_value = *(int *)current->content;
+	while (current)
+	{
+		if (*(int *)current->content < min_value)
+			min_value = *(int *)current->content;
+		current = current->next;
+	}
+	return (min_value);
 }
 
 int find_max(t_stack *stack)
 {
-    t_list	*current;
-    int max_value;
+	t_list	*current;
+	int		max_value;
 
-    if (!stack || !stack->top)
-        return (INT_MIN); // Return INT_MIN if the stack is empty
-    current = stack->top;
-    max_value = *(int *)current->content;
-
-    while (current)
-    {
-        if (*(int *)current->content > max_value)
-            max_value = *(int *)current->content;
-        current = current->next;
-    }
-    return (max_value);
+	if (!stack || !stack->top)
+		return (-1); // Return an invalid value if the stack is empty
+	current = stack->top;
+	max_value = *(int *)current->content;
+	while (current)
+	{
+		if (*(int *)current->content > max_value)
+			max_value = *(int *)current->content;
+		current = current->next;
+	}
+	return (max_value);
 }
 
 int find_max_index(t_stack *stack)
 {
-    t_list	*current;
-    int max_index = 0;
-    int max_value;
-    int index = 0;
+	t_list	*current;
+	int		max_index = 0;
+	int		max_value;
+	int		index = 0;
 
-    if (!stack || !stack->top)
-        return (-1); // Return -1 if the stack is empty
-
-    current = stack->top;
-    max_value = *(int *)current->content;
-
-    while (current)
-    {
-        if (*(int *)current->content > max_value)
-        {
-            max_value = *(int *)current->content;
-            max_index = index;
-        }
-        index++;
-        current = current->next;
-    }
-    return (max_index);
+	if (!stack || !stack->top)
+		return (-1);
+	current = stack->top;
+	max_value = *(int *)current->content;
+	while (current)
+	{
+		if (*(int *)current->content > max_value)
+		{
+			max_value = *(int *)current->content;
+			max_index = index;
+		}
+		index++;
+		current = current->next;
+	}
+	return (max_index);
 }

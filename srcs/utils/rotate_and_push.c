3ca9c420cfa5d0e284d413ac26d6b4d465931554
Christian <115067566+kitearuba@users.noreply.github.com>
2025-01-04 23:57:16 +0100

    Update rotate_and_push.c

--- srcs/utils/rotate_and_push.c
+++ srcs/utils/rotate_and_push.c
@@ -1,1 +1,18 @@
-g
+/* ************************************************************************** */
+/*                      Helper Functions for Turkish Sort                    */
+/* ************************************************************************** */
+
+
+/**
+ * rotate_and_push - Rotates stack A to optimize element placement in stack B.
+ * @a: Pointer to stack A.
+ * @b: Pointer to stack B.
+ */
+void rotate_and_push(t_stack *a, t_stack *b)
+{
+	int target_index;
+
+	target_index = find_optimal_rotation(a, b);
+	move_to_top(a, target_index);
+	pb(a, b);
+}
